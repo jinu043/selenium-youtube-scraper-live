e6b2d02549b69e8c18ea08608507e8b2aa50315b
jinu043 <93069439+jinu043@users.noreply.github.com>
2023-07-15 19:23:19 +0000

    Just started scraping. Called beautiful soup for parsing the html data

--- scraper.py
+++ scraper.py
@@ -1,1 +1,13 @@
-print("Hello world")
+youtube_trending_url = "https://www.youtube.com/feed/trending"
+
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get(youtube_trending_url)
+
+with open("trending.html", "w") as f:
+  f.write(response.text)
+
+doc = BeautifulSoup(response.text, "html.parser")
+
+print("Page title: ", doc.title)
